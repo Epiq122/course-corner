72264711b38b76cd3a1224255aba8c74178ea8d3
Robert Gleason <l2ob122@gmail.com>
2023-09-21 21:51:58 -0700

    annoted and added orm for the user class

--- backend/admin/src/main/java/dev/robglason/admin/entity/User.java
+++ backend/admin/src/main/java/dev/robglason/admin/entity/User.java
@@ -1,21 +1,41 @@
 package dev.robglason.admin.entity;
 
+import jakarta.persistence.*;
+
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long userId;
+
+    @Basic
+    @Column(name = "email", nullable = false, length = 45, unique = true)
     private String email;
+    @Basic
+    @Column(name = "password", nullable = false, length = 64)
     private String password;
 
     // relationship - each user has a set of roles
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role",
+            joinColumns = {@JoinColumn(name = "user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_id")})
     private Set<Role> roles = new HashSet<>();
 
     // relationship each user and be either a student or instructor
+
+    @OneToOne(mappedBy = "user")
     private Student student;
 
+    @OneToOne(mappedBy = "user")
     private Instructor instructor;
 
     public User() {
