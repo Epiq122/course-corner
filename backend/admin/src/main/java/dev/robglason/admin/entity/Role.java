a6c7d86e5ef9db9cc4edd7ddbc95c5c8ffa9dc1f
Robert Gleason <l2ob122@gmail.com>
2023-09-21 21:55:28 -0700

    annoted and added orm for the role class

--- backend/admin/src/main/java/dev/robglason/admin/entity/Role.java
+++ backend/admin/src/main/java/dev/robglason/admin/entity/Role.java
@@ -1,16 +1,27 @@
 package dev.robglason.admin.entity;
 
+import jakarta.persistence.*;
+
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+
+@Entity
+@Table(name = "roles")
 public class Role {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id", nullable = false)
     private Long roleId;
+    @Basic
+    @Column(name = "name", nullable = false, length = 45, unique = true)
     private String name;
 
     // relationship - a role can be given to a group of users
 
+    @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
     private Set<User> users = new HashSet<>();
 
     public Role(String name) {
@@ -62,7 +73,7 @@ public class Role {
         return result;
     }
 
-    @OverrideE
+    @Override
     public String toString() {
         return "Role{" +
                 "roleId=" + roleId +
