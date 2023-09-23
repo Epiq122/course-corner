975ea9cf09ab4a2e4e49a30a3381e030e7276a41
Robert Gleason <l2ob122@gmail.com>
2023-09-22 22:18:32 -0700

    chnaged the return type in course to page and pageable and added another field

--- backend/admin/src/main/java/dev/robglason/admin/AdminApplication.java
+++ backend/admin/src/main/java/dev/robglason/admin/AdminApplication.java
@@ -1,42 +1,15 @@
 package dev.robglason.admin;
 
-import dev.robglason.admin.dao.*;
-import dev.robglason.admin.utility.OperationUtility;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdminApplication implements CommandLineRunner {
+public class AdminApplication {
 
-    @Autowired
-    private UserDao userDao;
-
-    @Autowired
-    private CourseDao courseDao;
-
-    @Autowired
-    private InstructorDao instructorDao;
-
-    @Autowired
-    private RoleDao roleDao;
-
-    @Autowired
-    private StudentDao studentDao;
 
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-//        OperationUtility.usersOperation(userDao);
-//        OperationUtility.rolesOperations(roleDao);
-//        OperationUtility.assignRolesToUsers(userDao, roleDao);
-//        OperationUtility.instructorsOperations(userDao, instructorDao, roleDao);
-//        OperationUtility.studentOperations(userDao, studentDao, roleDao);
-        OperationUtility.coursesOperations(courseDao, instructorDao, studentDao);
 
-    }
 }
--- backend/admin/src/main/java/dev/robglason/admin/dao/CourseDao.java
+++ backend/admin/src/main/java/dev/robglason/admin/dao/CourseDao.java
@@ -1,20 +1,28 @@
 package dev.robglason.admin.dao;
 
 import dev.robglason.admin.entity.Course;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-
 public interface CourseDao extends JpaRepository<Course, Long> {
 
 
-    List<Course> findCoursesByCourseNameContains(String keyword);
+    Page<Course> findCoursesByCourseNameContains(String keyword, Pageable pageable);
 
 
     // Retrieve courses for a specific student based on their studentId
     @Query(value = "select * from courses as c where c.course_id in (select e.course_id from enrolled_in as e where e.student_id = :studentId)", nativeQuery = true)
-    List<Course> getCourseByStudentId(@Param("studentId") Long studentId);
+    Page<Course> getCourseByStudentId(@Param("studentId") Long studentId8, Pageable pageable);
 
+
+    // view the courses there not enrolled in
+    @Query(value = "select * from courses as c where c.course_id not in(select e.course_id from enrolled_in as e where e.student_id = :studentId)", nativeQuery = true)
+    Page<Course> getNotEnrolledInCoursesByStudentId(@Param("studentId") Long studentId8, Pageable pageable);
+
+    // get courses by instructor id
+    @Query(value = "select c from Course as c where c.instructor.instructorId = :instructorId")
+    Page<Course> getCourseByInstructorId(@Param("instructorId") Long instructorId, Pageable pageable);
 }
