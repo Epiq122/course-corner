03b6a0003dd96ca5db322f35b37d5b3062c6e2b4
Robert Gleason <l2ob122@gmail.com>
2023-09-22 22:23:59 -0700

    adeed Page to the student, instructor

--- backend/admin/src/main/java/dev/robglason/admin/dao/InstructorDao.java
+++ backend/admin/src/main/java/dev/robglason/admin/dao/InstructorDao.java
@@ -1,6 +1,8 @@
 package dev.robglason.admin.dao;
 
 import dev.robglason.admin.entity.Instructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,7 +14,7 @@ public interface InstructorDao extends JpaRepository<Instructor, Long> {
 
     // gets the instructor by their name (first or last)
     @Query(value = "select i from Instructor as i where i.firstName like %:name% or i.lastName like %:name%")
-    List<Instructor> findInstructorByName(@Param("name") String name);
+    Page<Instructor> findInstructorByName(@Param("name") String name, PageRequest pageRequest);
 
 
     @Query(value = "select i from Instructor as i where i.user.email = :email")
--- backend/admin/src/main/java/dev/robglason/admin/dao/StudentDao.java
+++ backend/admin/src/main/java/dev/robglason/admin/dao/StudentDao.java
@@ -1,6 +1,8 @@
 package dev.robglason.admin.dao;
 
 import dev.robglason.admin.entity.Student;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,7 +12,8 @@ import java.util.List;
 public interface StudentDao extends JpaRepository<Student, Long> {
 
     @Query(value = "select s from Student as s where s.firstName like %:name% or s.lastName like %:name%")
-    List<Student> findStudentsByNameContains(@Param("name") String name);
+    Page<Student> findStudentsByNameContains(@Param("name") String name, PageRequest pageRequest);
+
 
     @Query(value = "select s from Student as s where s.user.email = :email")
     List<Student> findStudentsByEmail(@Param("email") String email);
