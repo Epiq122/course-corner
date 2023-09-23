96469eafed32f3cf64fd644017b594444e0b8d32
Robert Gleason <l2ob122@gmail.com>
2023-09-22 19:31:12 -0700

    added role and user dao

---
+++ backend/admin/src/main/java/dao/RoleDao.java
@@ -0,0 +1,9 @@
+package dao;
+
+import dev.robglason.admin.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleDao extends JpaRepository<Role, Long> {
+
+    Role findByName(String name);
+}
---
+++ backend/admin/src/main/java/dao/User.java
@@ -0,0 +1,10 @@
+package dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface User extends JpaRepository<User, Long> {
+
+
+    User findByEmail(String email);
+    
+}
