0cf6bfcd4f7181e8fab17dfce81177103729ac44
Robert Gleason <l2ob122@gmail.com>
2023-09-23 16:58:55 -0700

    created instructor controller

---
+++ backend/admin/src/main/java/dev/robglason/admin/web/InstructorRestController.java
@@ -0,0 +1,71 @@
+package dev.robglason.admin.web;
+
+import dev.robglason.admin.dto.CourseDTO;
+import dev.robglason.admin.dto.InstructorDTO;
+import dev.robglason.admin.entity.User;
+import dev.robglason.admin.service.CourseService;
+import dev.robglason.admin.service.InstructorService;
+import dev.robglason.admin.service.UserService;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/instructors")
+public class InstructorRestController {
+    private InstructorService instructorService;
+
+    private UserService userService;
+
+    private CourseService courseService;
+
+    public InstructorRestController(InstructorService instructorService, UserService userService, CourseService courseService) {
+        this.instructorService = instructorService;
+        this.userService = userService;
+        this.courseService = courseService;
+    }
+
+    @GetMapping
+    public Page<InstructorDTO> searchInstructors(@RequestParam(name = "keyword", defaultValue = "") String keyword,
+                                                 @RequestParam(name = "page", defaultValue = "0") int page,
+                                                 @RequestParam(name = "size", defaultValue = "5") int size) {
+        return instructorService.findInstructorsByName(keyword, page, size);
+    }
+
+    @GetMapping("/all")
+    public List<InstructorDTO> findAllInstructors() {
+        return instructorService.getInstructors();
+    }
+
+    @DeleteMapping("/{instructorId}")
+    public void deleteInstructor(@PathVariable Long instructorId) {
+        instructorService.removeInstructor(instructorId);
+    }
+
+    @PostMapping
+    public InstructorDTO saveInstructor(@RequestBody InstructorDTO instructorDTO) {
+        User user = userService.loadUserByEmail(instructorDTO.getUser().getEmail());
+        if (user != null) throw new RuntimeException("Email Already Exist");
+        return instructorService.createInstructor(instructorDTO);
+    }
+
+    @PutMapping("/{instructorId}")
+    public InstructorDTO updateInstructor(@RequestBody InstructorDTO instructorDTO, @PathVariable Long instructorId) {
+        instructorDTO.setInstructorId(instructorId);
+        return instructorService.updateInstructor(instructorDTO);
+    }
+
+    @GetMapping("/{instructorId}/courses")
+    public Page<CourseDTO> coursesByInstructorId(@PathVariable Long instructorId,
+                                                 @RequestParam(name = "page", defaultValue = "0") int page,
+                                                 @RequestParam(name = "size", defaultValue = "5") int size) {
+        return courseService.fetchCoursesForInstructor(instructorId, page, size);
+    }
+
+    @GetMapping("/find")
+    public InstructorDTO loadInstructorByEmail(@RequestParam(name = "email", defaultValue = "") String email) {
+        return instructorService.loadInstructorByEmail(email);
+    }
+}
