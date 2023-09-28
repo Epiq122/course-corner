dcc9a8a7223deabdf8f9c250d9ab3e1d232daca4
Robert Gleason <l2ob122@gmail.com>
2023-09-28 13:24:52 -0700

    added pagination and listing all students

--- frontend/frontend/src/app/components/students/students.component.ts
+++ frontend/frontend/src/app/components/students/students.component.ts
@@ -1,27 +1,60 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { catchError, Observable, throwError } from 'rxjs';
+import { PageResponse } from '../../model/page.response.model';
+import { Student } from '../../model/student.model';
+import { StudentsService } from '../../services/students.service';
 
 @Component({
   selector: 'app-students',
   templateUrl: './students.component.html',
-  styleUrls: ['./students.component.css']
+  styleUrls: ['./students.component.css'],
 })
-
-
 export class StudentsComponent implements OnInit {
+  searchFormGroup!: FormGroup;
+  studentFormGroup!: FormGroup;
+  pageStudents$!: Observable<PageResponse<Student>>;
+  students$!: Observable<Array<Student>>;
+  errorMessage!: string;
+  submitted: boolean = false;
+  currentPage: number = 0;
+  pageSize: number = 3;
+  errorStudentMessage!: string;
 
-
-  // constructor() { }
-  constructor(private modalService: NgbModal) {}
+  constructor(
+    private modalService: NgbModal,
+    private fb: FormBuilder,
+    private studentService: StudentsService
+  ) {}
 
   ngOnInit(): void {
+    this.searchFormGroup = this.fb.group({
+      keyword: this.fb.control(''),
+    });
+    this.handleSearchStudents();
   }
 
+  getModal(content: any) {
+    this.submitted = false;
 
-  getModal(content: any){
-    this.modalService.open(content, { size: 'xl' })
-    console.log("Hello world")
+    this.modalService.open(content, { size: 'xl' });
   }
 
+  handleSearchStudents() {
+    let keyword = this.searchFormGroup.value.keyword;
+    this.pageStudents$ = this.studentService
+      .searchStudents(keyword, this.currentPage, this.pageSize)
+      .pipe(
+        catchError((err) => {
+          this.errorMessage = err.message;
+          return throwError(err);
+        })
+      );
+  }
 
+  gotoPage(page: number) {
+    this.currentPage = page;
+    this.handleSearchStudents();
+  }
 }
---
+++ frontend/frontend/src/app/services/students.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Student } from '../model/student.model';
+import { Instructor } from '../model/instructor.model';
+import { environment } from '../../environments/environment';
+import { PageResponse } from '../model/page.response.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StudentsService {
+  constructor(private http: HttpClient) {}
+
+  public fetchAllStudents(): Observable<Array<Student>> {
+    return this.http.get<Array<Student>>(
+      environment.backendHost + '/students/all'
+    );
+  }
+
+  public searchStudents(
+    keyword: string,
+    currentPage: number,
+    pageSize: number
+  ): Observable<PageResponse<Student>> {
+    return this.http.get<PageResponse<Student>>(
+      environment.backendHost +
+        '/students?keyword=' +
+        keyword +
+        '&page=' +
+        currentPage +
+        '&size=' +
+        pageSize
+    );
+  }
+}
